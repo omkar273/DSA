69266f9f78c54205893a988db0ce04c84598b670
omkar273 <omkarsonawane622@gmail.com>
2023-06-28 19:44:31 +0530

    patterns question finished

---
+++ dsa codes/1. patterns/square_pattern.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+function for printing a pattern like
+1  2  3  4
+1  2  3  4
+1  2  3  4
+1  2  3  4
+*/
+void pattern1(int n) {
+    for (size_t i = 1; i <= n; i++) {
+        for (size_t j = 1; j <= n; j++) {
+            cout << j << "  ";
+        }
+        cout << endl;
+    }
+}
+
+/*
+function for printing a pattern like
+4  3  2  1
+4  3  2  1
+4  3  2  1
+4  3  2  1
+*/
+void pattern2(int n) {
+    for (size_t i = 1; i <= n; i++) {
+        for (size_t j = n; j > 0; j--) {
+            cout << j << "  ";
+        }
+        cout << endl;
+    }
+}
+
+/*
+function for printing a pattern like
+1  2   3  4
+5  6   7  8
+9  10  11  12
+*/
+void pattern3(int n) {
+    int count = 1;
+    for (size_t i = 1; i <= n; i++) {
+        for (size_t j = 1; j <= n; j++) {
+            cout << count++ << "  ";
+        }
+        cout << endl;
+    }
+}
+
+/*
+function for printing a pattern like
+A  A  A  A
+B  B  B  B
+C  C  C  C
+D  D  D  D
+*/
+void pattern4(int n) {
+    for (size_t i = 0; i < n; i++) {
+        for (size_t j = 0; j < n; j++) {
+            cout << (char)('A' + i) << "  ";
+        }
+        cout << endl;
+    }
+}
+
+/*
+function for printing a pattern like
+A  B  C  D
+E  F  G  H
+I  J  K  L
+M  N  O  P
+*/
+void pattern5(int n) {
+    char c = 'A';
+    for (size_t i = 0; i < n; i++) {
+        for (size_t j = 0; j < n; j++) {
+            cout << c++ << "  ";
+        }
+        cout << endl;
+    }
+}
+
+
+int main() {
+    pattern5(6);
+    return 0;
+}
---
+++ dsa codes/1. patterns/triangle_pattern.cpp
@@ -0,0 +1,120 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+function for printing a pattern like
+*
+***
+****
+******
+*/
+void pattern1(int n) {
+    for (size_t i = 1; i <= n; i++) {
+        for (size_t j = 1; j <= i; j++) {
+            cout << "*  ";
+        }
+        cout << endl;
+    }
+}
+
+/*
+function for printing a pattern like
+A
+B C
+D E F
+G H I J
+*/
+void pattern2(int n) {
+    char c = 'A';
+    for (size_t i = 0; i < n; i++) {
+        for (size_t j = 0; j < i; j++) {
+            cout << c++ << "  ";
+        }
+        cout << endl;
+    }
+}
+
+/*
+function for printing a pattern like
+D
+C D
+B C D
+A B C D
+*/
+void pattern3(int n) {
+    for (size_t i = 1; i <= n; i++) {
+        char c = 'A' + n - i;
+        for (size_t j = 0; j < i; j++) {
+            cout << char(c + j) << " ";
+        }
+        cout << endl;
+    }
+}
+
+/*
+function for printing a pattern like
+---    *
+--   * *
+-  * * *
+ * * * *
+*/
+void pattern4(int n) {
+    for (size_t i = 1; i <= n; i++) {
+        int no_spaces = n - i;
+        for (size_t j = 1; j <= no_spaces; j++) {
+            cout << "  ";
+        }
+        for (size_t k = 1; k <= i; k++) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+
+/*
+function for printing a pattern like
+*  *  *  *
+*  *  *
+*  *
+*
+
+*/
+void pattern5(int n) {
+    for (size_t i = n; i > 0; i--) {
+        for (size_t j = 0; j < i; j++) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+
+/*
+function for printing a pattern like
+    --- 1
+    --1 2 1
+    1 2 3 2 1
+  1 2 3 4 3 2 1
+
+*/
+void pattern6(int n) {
+    for (size_t i = 1; i <= n; i++) {
+        // for spaces
+        for (size_t j = 0; j < n - i; j++) {
+            cout << "  ";
+        }
+        int count = 1;
+        for (size_t k = 1; k <= i; k++) {
+            cout << k << " ";
+        }
+        for (size_t l = i - 1; l > 0; l--) {
+            cout << l << " ";
+        }
+
+        cout << endl;
+    }
+}
+
+int main() {
+    pattern6(5);
+    return 0;
+}
